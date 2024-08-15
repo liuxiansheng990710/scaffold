151ec10b55f58b1b0a37efa89b4230bc184a67ef
21 <1069823632@qq.com>
2024-08-15 16:37:14 +0800

    初始化

---
+++ src/main/java/com/example/boot3scaffold/Boot3ScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot3scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot3ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot3ScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boot3scaffold/Boot3ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot3scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot3ScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
