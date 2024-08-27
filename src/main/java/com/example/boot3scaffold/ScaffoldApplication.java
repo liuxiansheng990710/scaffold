01ad495ba8ba9acdce89d2b852e232b852cdfc81
21 <1069823632@qq.com>
2024-08-27 10:00:44 +0800

    feat(初始化) ： 初始化部分依赖
    1. 添加springboot部分依赖
    2. 增加统一依赖管理
    3. 添加mongo yaml配置

--- src/main/java/com/example/boot3scaffold/ScaffoldApplication.java
+++ src/main/java/com/example/boot3scaffold/ScaffoldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot3ScaffoldApplication {
+public class ScaffoldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Boot3ScaffoldApplication.class, args);
+        SpringApplication.run(ScaffoldApplication.class, args);
     }
 
 }
